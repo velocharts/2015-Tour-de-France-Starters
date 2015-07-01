eac71d8945edc62b25a8e92378c679a08b5cdb16
velocharts <velocharts@users.noreply.github.com>
2015-07-01 21:46:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
 $(function () {
 
-    var data =[{"name":"Albania","code":"AL","z":1},
-{"name":"Argentina","code":"AR","z":1},
-{"name":"Australia","code":"AU","z":11},
-{"name":"Austria","code":"AT","z":1},
-{"name":"Belarus","code":"BY","z":3},
-{"name":"Belgium","code":"BE","z":13},
-{"name":"Brazil","code":"BR","z":1},
-{"name":"Bulgaria","code":"BG","z":1},
+    var data =[{"name":"France","code":"FR","z":41},
+{"name":"Netherlands","code":"NL","z":20},
+{"name":"Italy","code":"IT","z":16},
+{"name":"Spain","code":"ES","z":15},
+{"name":"Belgium","code":"BE","z":11},
+{"name":"Germany","code":"DE","z":10},
+{"name":"Australia","code":"AU","z":10},
+{"name":"United Kingdom","code":"GB","z":10},
+{"name":"Switzerland","code":"CH","z":10},
+{"name":"Colombia","code":"CO","z":6},
+{"name":"Portugal","code":"PT","z":4},
+{"name":"Czech Republic","code":"CZ","z":4},
+{"name":"South Africa","code":"ZA","z":4},
+{"name":"Poland","code":"PL","z":4},
+{"name":"United States","code":"US","z":3},
+{"name":"Austria","code":"AT","z":3},
+{"name":"Denmark","code":"DK","z":3},
+{"name":"Luxembourg","code":"LU","z":3},
+{"name":"Ireland","code":"IE","z":3},
 {"name":"Canada","code":"CA","z":2},
-{"name":"China","code":"CN","z":2},
-{"name":"Colombia","code":"CO","z":7},
-{"name":"Costa Rica","code":"CR","z":1},
-{"name":"Czech Republic","code":"CZ","z":3},
-{"name":"Denmark","code":"DK","z":2},
-{"name":"Estonia","code":"EE","z":1},
-{"name":"Ethiopia","code":"ET","z":1},
-{"name":"Finland","code":"FI","z":1},
-{"name":"France","code":"FR","z":15},
-{"name":"Germany","code":"DE","z":8},
-{"name":"Italy","code":"IT","z":60},
-{"name":"Japan","code":"JP","z":2},
+{"name":"New Zealand","code":"NZ","z":2},
+{"name":"Estonia","code":"EE","z":2},
+{"name":"Eritrea","code":"ER","z":2},
+{"name":"Norway","code":"NO","z":2},
 {"name":"Kazakhstan","code":"KZ","z":1},
-{"name":"Latvia","code":"LV","z":1},
-{"name":"Netherlands","code":"NL","z":12},
-{"name":"New Zealand","code":"NZ","z":3},
-{"name":"Panama","code":"PA","z":1},
-{"name":"Poland","code":"PL","z":7},
-{"name":"Portugal","code":"PT","z":3},
-{"name":"Romania","code":"RO","z":1},
-{"name":"Russian Federation","code":"RU","z":9},
-{"name":"Slovenia","code":"SI","z":3},
-{"name":"Spain","code":"ES","z":11},
-{"name":"Sweden","code":"SE","z":1},
-{"name":"Switzerland","code":"CH","z":3},
-{"name":"United States","code":"US","z":5},
-{"name":"Venezuela","code":"VE","z":1}];
+{"name":"Slovenia","code":"SI","z":1},
+{"name":"Ukraine","code":"UA","z":1},
+{"name":"Argentina","code":"AR","z":1},
+{"name":"Lithuania","code":"LT","z":1},
+{"name":"Croatia","code":"HR","z":1},
+{"name":"Slovakia","code":"SK","z":1},
+{"name":"Russia","code":"RU","z":1}];
     
   
         
@@ -43,7 +39,7 @@ $(function () {
         $('#container').highcharts('Map', {
             
             title : {
-                text : '2015 Tour de France Riders by Nationality',
+                text : 'BlackRock Sovereign Risk Index',
                 style: {fontSize: '20px', fontFamily: 'Arial, Helvetica, sans-serif', color: '#4B4B4B'}
             },
   legend: {
@@ -73,7 +69,7 @@ $(function () {
                 enableMouseTracking: false
             }, {
                 type: 'mapbubble',
-                    color: 'orange',
+                 color: 'orange',
                  marker: {
             //  fillOpacity:0.3
                fillColor: 'rgb(255,255,0)'
